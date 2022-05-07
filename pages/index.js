ce6abc73eb4c6a4d12dae60708489c11cb0b7ab0
Reasoft76 <85739126+Reasoft76@users.noreply.github.com>
2022-05-07 12:39:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+    div.style.color = "White";
       <Head>
         <title>Sinfonia D'autunno!</title>
         <link rel="icon" href="/notfavicon.ico" />
