d2b6d56b7938c3728803d2f3cfca9729e53bfbad
Reasoft76 <85739126+Reasoft76@users.noreply.github.com>
2022-05-08 22:01:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ export default function Home() {
         <title>Sinfonia D'autunno!</title>
         <h1>Sinfonia D'autunno!</h1>
         <link rel="icon" href="/notfavicon.ico" />
-      </Head>
-      <main>
         <p>
           Cari esploratori e guide, siete tutti invitati alla nostra impresa di reparto: SINFONIA
           D’AUTUNNO, la più grande sfida musicale fra le band di tutti i reparti! l’evento si svolgerà
@@ -20,7 +18,7 @@ export default function Home() {
           vi attendiamo numerosi in uniforme per far conoscere il proprio reparto e il vostro amore per lo scoutismo
           BUONA CACCIA
         </p>
-      </main>
+       </Head>
       <a href="https://www.instagram.com/_concerto_sinfoniadautunno_/"><button>Instagram</button></a>
       <a href="https://www.facebook.com/profile.php?id=100080924825776"><button>Facebook</button></a>
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSelalrEpc82UmPNQ6t_hPbXFHao0hHF9UoGniZ3hdJBVO36dA/viewform"><button>Modulo d' iscrizione</button></a>
