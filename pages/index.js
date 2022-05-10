6384027f19409e90974dfaa173f26b76c1dd6fc1
Reasoft76 <85739126+Reasoft76@users.noreply.github.com>
2022-05-10 18:39:25 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,6 @@ export default function Home() {
         <p>8 L’iscrizione dovrà essere inviata entro il 21 maggio con tutte le informazioni del modulo ( tutte le domande che trovate nel modulo sono obbligatorie)</p>
         <p>9 Il giorno del concerto ogni persona avrà un fogliettino dove scriverà il nome della sua band preferita che poi sarà messo in un grande scatolone per poi eleggere il vincitore; Ogni persona potrà votare solo una volta.</p>
         <p>10 Per partecipare al concerto è obbligatoria l’uniforme ( solo i componenti della band)</p>
-        </p>
        </Head>
     </div>
   )
